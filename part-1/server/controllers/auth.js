b0f0ad65c1bf217aafdbf9c12d84a068b6139320
Alegraleeson <wingmonkee@yahoo.com>
2022-07-14 16:46:34 -0500

    first commit

---
+++ part-1/server/controllers/auth.js
@@ -0,0 +1,51 @@
+const bcryptjs = require('bcryptjs');
+
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      // console.log('Logging In User')
+      // console.log(req.body)
+      const { username, password } = req.body
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+          const authenticated = bcryptjs.compareSync(password, users[i].password);
+          
+          if(authenticated === true){
+
+            let userReturn = {...users[i]};
+            delete userReturn.hashed;
+          
+          res.status(200).send(userReturn)
+        } 
+      }
+
+
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+        // console.log('Registering User')
+        // console.log(req.body)
+        const { username, email, firstName, lastName, password, password2 } = req.body
+        const salt = bcryptjs.genSaltSync(5);
+        // NUMBER = HOW MANY TIMES TO ENCRIPT IT
+        const hashed = bcryptjs.hashSync(password, salt);
+        // console.log(hashed)
+let user = {
+  username,
+  email,
+  firstName,
+  lastName,
+  password: hashed,
+  password2: hashed,
+}
+      
+
+        users.push(user)
+        let userReturn = {...user}
+        delete userReturn.hashed
+        res.status(200).send(userReturn)
+    }
+}
+
